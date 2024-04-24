e1e7f1f04853ae0528e5570e965fd8e2acc8e35b
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-24 11:04:44 +0800

    feat:🚀新增 Java11 支持

---
+++ src/main/java11/java/net/http/HttpHeaders.java
@@ -0,0 +1,13 @@
+package java.net.http;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public final class HttpHeaders {
+
+	public Map<String,List<String>> map() {
+		return Collections.emptyMap();
+	}
+
+}
---
+++ src/main/java11/java/net/http/HttpRequest.java
@@ -0,0 +1,51 @@
+package java.net.http;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.time.Duration;
+
+/**
+ * For the Java 8 compatibility when compiled with JDK 11+.
+ *
+ * @author L.cm
+ */
+public class HttpRequest {
+
+	public static Builder newBuilder() {
+		return null;
+	}
+
+	public interface BodyPublisher {
+	}
+
+	public static class BodyPublishers {
+		private BodyPublishers() {
+
+		}
+		public static BodyPublisher ofString(String body) {
+			return null;
+		}
+
+		public static BodyPublisher ofString(String s, Charset charset) {
+			return null;
+		}
+
+		public static BodyPublisher noBody() {
+			return null;
+		}
+	}
+
+	public interface Builder {
+		Builder uri(URI uri);
+
+		Builder header(String name, String value);
+
+		Builder timeout(Duration duration);
+
+		Builder GET();
+
+		Builder POST(BodyPublisher bodyPublisher);
+
+		HttpRequest build();
+	}
+}
